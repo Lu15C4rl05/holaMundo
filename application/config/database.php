be653c7d33f77ae368dbf05e186f5d2c2d76cc1d
LU15C4RL05 <lucasruales@gmail.com>
2018-03-05 11:40:41 -0500

    Cambié el host de la base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.0.101',
 	'username' => 'root',
 	'password' => 'ROOT',
 	'database' => 'appboletos',
